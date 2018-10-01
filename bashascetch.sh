a235fc3f91e260a02fed05a14eb1922ef0a1d00d
James Elsdon <Jamesys@gmail.com>
2018-10-01 21:35:21 +1000

    Add rudimentry game board boarder

--- bashascetch.sh
+++ bashascetch.sh
@@ -33,6 +33,59 @@ function getWindowSize() {
   WINDOW_HEIGHT=$(getWindowHeight)
 }
 
+function drawBoxCorner() {
+  echo -n '+'
+}
+
+function drawBoxDash() {
+  echo -n '-'
+}
+
+function drawBoxPipe() {
+  echo -n '|'
+}
+
+function drawBoxBar() {
+  local x_pos=0
+  local y_pos=0
+  local window_width=$WINDOW_WIDTH
+  drawBoxCorner
+  let window_width=window_width-2
+  while [ $x_pos -lt $window_width ]
+  do
+    drawBoxDash
+    let x_pos=x_pos+1
+  done	
+  drawBoxCorner
+}
+
+function drawBoxSides() {
+  local y_pos=1
+  getWindowSize
+
+  while [ $y_pos -lt $((WINDOW_HEIGHT-1)) ]
+  do
+    tput cup ${y_pos} 0
+    drawBoxPipe
+    tput cup ${y_pos} $WINDOW_WIDTH
+    drawBoxPipe
+    let y_pos=y_pos+1
+  done
+}
+
+function drawBorder() {
+  local x_pos=0
+  local y_pos=0
+  # Draw the top line
+  tput cup ${y_pos} ${x_pos}
+  drawBoxBar
+  drawBoxSides
+  drawBoxBar
+
+}
+
 setWindowSize $WIDTH $HEIGHT
 clearWindow
 getWindowSize
+drawBorder
+sleep 10
