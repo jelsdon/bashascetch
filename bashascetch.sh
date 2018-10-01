2926e500bd4c5cb24285f3e6ad97a1506840638d
James Elsdon <Jamesys@gmail.com>
2018-10-01 21:06:44 +1000

    Add board size detection

--- bashascetch.sh
+++ bashascetch.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
+shopt -s checkwinsize
 WIDTH=80
 HEIGHT=24
+WINDOW_HEIGHT=""
+WINDOW_WIDTH=""
+
 
 function setWindowSize() {
   local x=$1; shift
@@ -14,5 +19,20 @@ function clearWindow() {
   printf "\033c"
 }
 
+function getWindowWidth() {
+  echo $COLUMNS
+}
+
+function getWindowHeight() {
+  echo $LINES
+}
+
+function getWindowSize() {
+  (:) # Subshell sets $LINES and $COLUMNS
+  WINDOW_WIDTH=$(getWindowWidth)
+  WINDOW_HEIGHT=$(getWindowHeight)
+}
+
 setWindowSize $WIDTH $HEIGHT
 clearWindow
+getWindowSize
